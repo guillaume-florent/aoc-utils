a11ee4f9ff6746fa876353a58b300d4b8fe12561
Guillaume Florent <guillaume.florent@tutanota.com>
2018-04-06 01:25:03 +0200

    Version is 18.0.0

--- aocutils/__init__.py
+++ aocutils/__init__.py
@@ -13,9 +13,9 @@ from __future__ import unicode_literals
 __name__ = "aocutils"
 __description__ = "High level API for PythonOCC "
 
-__version__ = "0.5.6"
+__version__ = "18.0.0"
 __author__ = "Guillaume Florent, Adam Lange, Jelle Feringa, Thomas Paviot"
-__author_email__ = "florentsailing@gmail.com"
+__author_email__ = "guillaume.florent@tutanota.com"
 __license__ = 'GPL v3'
 __url__ = "https://github.com/guillaume-florent/aoc-utils"
 __download_url__ = "https://github.com/guillaume-florent/aoc-utils/releases/tag/" + __version__
